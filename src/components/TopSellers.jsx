34ac9c55dab0fdb6c2457549a07659a893de9c2d
April B <aprilt.breen@gmail.com>
2024-02-08 16:17:08 -0500

    update TopSellers

--- src/components/TopSellers.jsx
+++ src/components/TopSellers.jsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react";
+
 export default function TopSellers() {
+    let [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('/api/products/bestsellers')
+                console.log(response.body)
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                }
+                products = await response.json();
+                setProducts(products);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <>
-            <p>TopSellers</p>
+            <p>
+                {products.map((p) => {
+                    return (
+
+                        <h3>
+                            {p.Name}
+                        </h3>
+                    )
+                })}
+            </p>
         </>
     )
 }
--- src/server/server.js
+++ src/server/server.js
@@ -19,11 +19,21 @@ try {
     console.error("Cannot Connect", err)
 }
 
+// get all products
 app.get('/api/products', async function (req, res) {
     const productsCol = db.collection('products');
-    const products = await productsCol.find().toArray();
+    const products = await productsCol.find()
     console.log('Products: ' + JSON.stringify(products));
     //return all the products
+    res.send(products)
+})
+// get best sellers
+app.get('/api/products/bestsellers', async function (req, res) {
+    const productsCol = db.collection('products');
+    // Filter documents where "Popularity" is greater than 3
+    const products = await productsCol.find({ "Popularity": { $gt: 3 } }).toArray()
+    console.log('Products: ' + JSON.stringify(products));
+    //return all the products with Popularity greater than 3
     res.send(products)
 })
 
